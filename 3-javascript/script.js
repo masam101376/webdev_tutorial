cb2c611d776fbc453a0738a1577934c2385f2047
Masahiro Muto <47169655+masam101376@users.noreply.github.com>
2020-11-20 15:41:02 +0900

    Add files for lesson 10; outer javascript

---
+++ 3-javascript/script.js
@@ -0,0 +1,16 @@
+
+            function average(a, b){
+                return((a+b)/2);
+            }
+
+            var x = 5;
+            var y = 10;
+            console.log(average(x, y));
+
+            var x = 8;
+            var y = 10;
+            console.log(average(x, y));
+
+            var x = 5;
+            var y = 19;
+            console.log(average(x, y));
